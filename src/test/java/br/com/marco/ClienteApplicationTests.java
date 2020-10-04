f192e839ea082ff66c239db709a5e2efaf23db19
masco54 <marcosilvacosta54@gmail.com>
2020-10-04 17:12:24 -0300

    add project

---
+++ src/test/java/br/com/marco/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
